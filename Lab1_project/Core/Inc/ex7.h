fd6fde178c276c2947527991335ce268d3e58979
Tien CSE <tien.nguyen11102004@hcmut.edu.vn>
2024-09-10 13:25:00 +0700

    update ex7

---
+++ Lab1_project/Core/Inc/ex7.h
@@ -0,0 +1,15 @@
+/*
+ * ex7.h
+ *
+ *  Created on: Sep 7, 2024
+ *      Author: thuyh
+ */
+
+#ifndef INC_ex7_H_
+#define INC_ex7_H_
+
+#include "main.h"
+#include "LED.h"
+void ex7_init();
+void ex7_run();
+#endif /* INC_ex7_H_ */
---
+++ Lab1_project/Core/Src/ex7.c
@@ -0,0 +1,11 @@
+/*
+ * ex7.c
+ *
+ *  Created on: Sep 7, 2024
+ *      Author: thuyh
+ */
+#include "ex7.h"
+void ex7_init(){}
+void ex7_run(){
+	clearAllClock(GPIOA, GREEN_1_Pin, YELLOW_1_Pin, RED_1_Pin, GREEN_2_Pin, YELLOW_2_Pin, RED_2_Pin, GREEN_3_Pin, YELLOW_3_Pin, RED_3_Pin, GREEN_4_Pin, YELLOW_4_Pin, RED_4_Pin);
+}
