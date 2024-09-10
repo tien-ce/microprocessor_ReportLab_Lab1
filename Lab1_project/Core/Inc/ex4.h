f199cc67c8f05cf16ef7b83a5d730a44edd5311b
Tien CSE <tien.nguyen11102004@hcmut.edu.vn>
2024-09-10 13:19:32 +0700

    update ex4

---
+++ Lab1_project/Core/Inc/ex4.h
@@ -0,0 +1,15 @@
+/*
+ * ex4.h
+ *
+ *  Created on: Sep 7, 2024
+ *      Author: thuyh
+ */
+
+#ifndef INC_EX4_H_
+#define INC_EX4_H_
+
+#include "main.h"
+#include "LED7_SEGMENT.h"
+void ex4_init();
+void ex4_run();
+#endif /* INC_EX4_H_ */
--- Lab1_project/Debug/Core/Src/ex4.d
+++ Lab1_project/Debug/Core/Src/ex4.d
@@ -1,4 +1,4 @@
-Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+Core/Src/ex4.o: ../Core/Src/ex4.c ../Core/Inc/ex4.h ../Core/Inc/main.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
  ../Core/Inc/stm32f1xx_hal_conf.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
@@ -21,8 +21,10 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
- ../Core/Inc/ex1.h ../Core/Inc/main.h ../Core/Inc/ex4.h \
- E:/Microprocesser-microcontroller/SOW/Ins/LED7_SEGMENT.h
+ E:/Microprocesser-microcontroller/SOW/Ins/LED7_SEGMENT.h \
+ ../Core/Inc/main.h
+
+../Core/Inc/ex4.h:
 
 ../Core/Inc/main.h:
 
@@ -70,10 +72,6 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
 
-../Core/Inc/ex1.h:
+E:/Microprocesser-microcontroller/SOW/Ins/LED7_SEGMENT.h:
 
 ../Core/Inc/main.h:
-
-../Core/Inc/ex4.h:
-
-E:/Microprocesser-microcontroller/SOW/Ins/LED7_SEGMENT.h:
