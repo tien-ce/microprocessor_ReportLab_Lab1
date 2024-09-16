b5d1c112d6a0eec26e0d5839c4de703cd0d58fee
Tien CSE <tien.nguyen11102004@hcmut.edu.vn>
2024-09-16 14:19:14 +0700

    ex10

--- Lab1_project/Debug/SOW/Src/subdir.mk
+++ Lab1_project/Debug/SOW/Src/subdir.mk
@@ -7,17 +7,20 @@
 C_SRCS += \
 E:/Microprocesser-microcontroller/SOW/Src/LED.c \
 E:/Microprocesser-microcontroller/SOW/Src/LED_7_SEGMENT.c \
-E:/Microprocesser-microcontroller/SOW/Src/Traffic_light.c 
+E:/Microprocesser-microcontroller/SOW/Src/Traffic_light.c \
+E:/Microprocesser-microcontroller/SOW/Src/software_timer.c 
 
 OBJS += \
 ./SOW/Src/LED.o \
 ./SOW/Src/LED_7_SEGMENT.o \
-./SOW/Src/Traffic_light.o 
+./SOW/Src/Traffic_light.o \
+./SOW/Src/software_timer.o 
 
 C_DEPS += \
 ./SOW/Src/LED.d \
 ./SOW/Src/LED_7_SEGMENT.d \
-./SOW/Src/Traffic_light.d 
+./SOW/Src/Traffic_light.d \
+./SOW/Src/software_timer.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
@@ -26,5 +29,7 @@ SOW/Src/LED.o: E:/Microprocesser-microcontroller/SOW/Src/LED.c SOW/Src/subdir.mk
 SOW/Src/LED_7_SEGMENT.o: E:/Microprocesser-microcontroller/SOW/Src/LED_7_SEGMENT.c SOW/Src/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103x6 -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I"E:/Microprocesser-microcontroller/SOW/Ins" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 SOW/Src/Traffic_light.o: E:/Microprocesser-microcontroller/SOW/Src/Traffic_light.c SOW/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103x6 -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I"E:/Microprocesser-microcontroller/SOW/Ins" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+SOW/Src/software_timer.o: E:/Microprocesser-microcontroller/SOW/Src/software_timer.c SOW/Src/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103x6 -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I"E:/Microprocesser-microcontroller/SOW/Ins" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
